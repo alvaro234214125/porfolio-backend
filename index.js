2f5f5d16e88e5849852e0ab16e681430fe5d4e05
alvaro234214125 <alvarocesardiazchang@gmail.com>
2025-06-10 23:14:10 -0500

    fix: use PORT from Render

--- index.js
+++ index.js
@@ -30,5 +30,6 @@ app.post("/api/contact", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
